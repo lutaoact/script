3dffdbc201778dab8b4f87b49c5791e2a2f4dd63
lutaoact <i@lutao.me>
2014-09-27 11:10:06 +0800

    The special bash newline char to send to sed: $'\n'

--- sed_use.sh
+++ sed_use.sh
@@ -2,3 +2,15 @@ sed -i '' "s/my/Hao Chen's/" pets.txt
 sed -i '' "3s/my/your/" pets.txt
 sed -i '' "3,6s/my/your/" pets.txt #只替换第3到第6行的文本
 sed -iold '1i\'$'\n''text to prepend'$'\n' file.txt
+
+#         sed -iold '1i\'$'\n''text to prepend'$'\n' file.txt
+#                      ^^^^^^^^                     ^
+#                     / |\|||/ \                    |__ No need to reopen
+#                     | | \|/  |                          string to sed
+#     Tells sed to    | |  |   |
+#    escape the next _/ |  |   +-----------------------------+
+#         char          |  +-------------+                   |
+#                       |                |                   |
+#                  Close string   The  special bash   Reopen string to
+#                     to sed       newline char to      send to sed
+#                                    send to sed
