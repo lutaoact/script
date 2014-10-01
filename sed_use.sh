07dad9cb988ae8a70efca8028bcb36789171e5b4
lutaoact <i@lutao.me>
2014-10-01 16:27:50 +0800

    sed = pets.txt | sed 'N;s/\n/'$'\t''/' > line_num_pets.txt

--- sed_use.sh
+++ sed_use.sh
@@ -30,3 +30,4 @@ sed -i '' '3,6 {/This/{/fish/d;};}' pets.txt
 sed -i '' '1,${/This/d;s/^ *//g;}' pets.txt
 sed -i '' -E '/dog/{N;N;N;s/(^|\n)/&# /g;}' pets.txt #BSD sed
 sed -i '' '/dog/,+3s/^/# /g' pets.txt #GNU sed
+sed = pets.txt | sed 'N;s/\n/'$'\t''/' > line_num_pets.txt
