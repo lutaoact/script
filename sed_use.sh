857a560cb2af89d1f836c95c6987fe5c47b28ae2
lutaoact <i@lutao.me>
2014-10-01 18:02:14 +0800

    sed '/'"$name"'/,/};/d' back_slash.txt

--- sed_use.sh
+++ sed_use.sh
@@ -33,3 +33,4 @@ sed -i '' '/dog/,+3s/^/# /g' pets.txt #GNU sed
 sed = pets.txt | sed 'N;s/\n/'$'\t''/' > line_num_pets.txt
 sed = my.txt | sed 'N; s/^/    /; s/\(.\{5,\}\)\n/\1 /' #对文件中的所有行编号（行号在左，文字左端对齐）。
 #sed = my.txt | sed -E 'N; s/(.*)\n/    \1 /' #貌似，我也可以这么写
+sed '/'"$name"'/,/};/d' back_slash.txt
