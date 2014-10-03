9d215768eb32d35284055fe34fa54eedcb4208a4
lutaoact <i@lutao.me>
2014-10-03 23:28:25 +0800

    awk '/WAIT/' netstat.txt

--- awk_use.sh
+++ awk_use.sh
@@ -3,7 +3,7 @@ awk '{printf "%-8s %-8s %-8s %-18s %-22s %-15s\n",$1,$2,$3,$4,$5,$6}' netstat.tx
 awk '$3 == 0 && $6 == "LAST_ACK"' netstat.txt
 awk '$3 > 0 && NR != 1 {print $3}' netstat.txt
 awk 'BEGIN{FS=":"} {print $1,$3,$6}' semi_colon_FS #awk -F: '{print $1,$3,$6}' semi_colon_FS
-
+awk '/WAIT/' netstat.txt
 
 # awk内建变量
 # $0  当前记录（这个变量中存放着整个行的内容）
