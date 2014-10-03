d87dee15f523fc170c65c64b3137f534acbe09c0
lutaoact <i@lutao.me>
2014-10-03 23:07:33 +0800

    awk '$3 > 0 && NR != 1 {print $3}' netstat.txt

--- awk_use.sh
+++ awk_use.sh
@@ -1,3 +1,4 @@
 awk '{print $1, $3}' netstat.txt
 awk '{printf "%-8s %-8s %-8s %-18s %-22s %-15s\n",$1,$2,$3,$4,$5,$6}' netstat.txt
 awk '$3 == 0 && $6 == "LAST_ACK"' netstat.txt
+awk '$3 > 0 && NR != 1 {print $3}' netstat.txt
