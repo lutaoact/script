399929099ad3e8f4da3db68aeea62282847d1e0a
lutaoact <i@lutao.me>
2014-10-03 23:20:32 +0800

    awk -F: '{print $1,$3,$6}' semi_colon_FS

--- awk_use.sh
+++ awk_use.sh
@@ -2,6 +2,7 @@ awk '{print $1, $3}' netstat.txt
 awk '{printf "%-8s %-8s %-8s %-18s %-22s %-15s\n",$1,$2,$3,$4,$5,$6}' netstat.txt
 awk '$3 == 0 && $6 == "LAST_ACK"' netstat.txt
 awk '$3 > 0 && NR != 1 {print $3}' netstat.txt
+awk 'BEGIN{FS=":"} {print $1,$3,$6}' semi_colon_FS #awk -F: '{print $1,$3,$6}' semi_colon_FS
 
 
 # awk内建变量
