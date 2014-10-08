7c90f7f66c78e9746c832dc2cda89085f2bcf737
lutaoact <i@lutao.me>
2014-10-08 11:42:53 +0800

    du_use.sh

---
+++ du_use.sh
@@ -0,0 +1,2 @@
+BLOCKSIZE  If the environment variable BLOCKSIZE is set, and the -k option is not specified, the block counts will be displayed in units of that size block.  If
+           BLOCKSIZE is not set, and the -k option is not specified, the block counts will be displayed in 512-byte blocks.
