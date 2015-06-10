efa7272aec16f55612d1edac5ef22ed770f36781
lutaoact <i@lutao.me>
2015-06-10 16:19:12 +0800

    postgres.sh

--- postgres.sh
+++ postgres.sh
@@ -1,2 +1,8 @@
 createuser --superuser -echo sequelize #创建超级用户
 createdb --username=sequelize development #利用sequelize用户创建数据库development
+
+\l #mysql> show databases;
+
+select pg_database_size('playboy'); #查看playboy数据库的大小
+select pg_database.datname, pg_database_size(pg_database.datname) AS size from pg_database; #查看所有数据库的大
+select pg_size_pretty(pg_database_size('playboy')); #以KB，MB，GB的方式来查看数据库大小  
