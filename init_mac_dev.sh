04387353331e10afed43fcce987fd035d4081b44
lutaoact <i@lutao.me>
2016-02-01 18:01:45 +0800

    init_mac_dev.sh remove redis-server

--- init_mac_dev.sh
+++ init_mac_dev.sh
@@ -1,3 +1,3 @@
-redis-server ~/some_config/redis.conf
+#redis-server ~/some_config/redis.conf
 #mongod --logappend --logpath /data/log/mongod.log --fork
 rm -rf /data/pid/polipo.pid && polipo -c ~/some_config/polipo.conf
