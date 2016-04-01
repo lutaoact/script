67651e00267c975345702bf5bd4e30f0c373f4fd
lutaoact <i@lutao.me>
2016-04-01 15:22:37 +0800

    lutaoact_functions.sh: svn_ci

--- lutaoact_functions.sh
+++ lutaoact_functions.sh
@@ -6,3 +6,13 @@ function gbk2utf8 {
   mv "$j" "$1"
   dos2unix -r "$1"
 }
+
+function svn_ci {
+  dir=$(pwd)
+  cd ~
+  rsync -rv --exclude=.git --exclude=.gitignore --exclude-from=node-server/.gitignore ~/node-server/ ~/Service/trunk/node-server/
+  cd ~/Service/trunk/node-server/
+  svn add --force .
+  svn ci -m "$1"
+  cd $dir
+}
