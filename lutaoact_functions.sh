65f009a26e9e679302546ca8dea24de756fb18b7
lutaoact <i@lutao.me>
2016-04-07 11:21:36 +0800

    lutaoact_functions.sh: sync_to sync_from sort_lines_by_length

--- lutaoact_functions.sh
+++ lutaoact_functions.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+function sort_lines_by_length {
+  if [ -z "$1" ]; then
+    echo "collection name is required"
+    exit 1
+  fi
+  cp "$1" /tmp/$(basename "$1")
+  awk '{print length, $0}' "$1" | sort -nk1 -s | cut -d" " -f2- > "${1}.tmp"
+  mv "${1}.tmp" "$1"
+}
+
 function gbk2utf8 {
   j="${1}_tmp"
   iconv -f GBK -t UTF-8 "$1" > "$j"
@@ -15,4 +25,38 @@ function svn_ci {
   svn add --force .
   svn ci -m "$1"
   cd $dir
+}
+
+function sync_to {
+  if [ -z "$1" ]; then
+    echo "collection name is required"
+    exit 1
+  fi
+  date=$(date +%F)
+  backup_file=${1}_$date.json
+  echo -e "mongoexport -d gpws -c $1 --jsonArray -o /data/backup/$backup_file"
+  mongoexport -d gpws -c "$1" --jsonArray -o /data/backup/$backup_file
+
+  echo "scp /data/backup/$backup_file node:/data/backup"
+  scp /data/backup/$backup_file node:/data/backup
+
+  echo "ssh node \"mongoimport -h mongo -d gpws -c $1 --jsonArray /data/backup/$backup_file\""
+  ssh node "mongoimport -h mongo -d gpws -c $1 --jsonArray /data/backup/$backup_file"
+}
+
+function sync_from {
+  if [ -z "$1" ]; then
+    echo "collection name is required"
+    exit 1
+  fi
+  date=$(date +%F)
+  backup_file=${1}_$date.json
+  echo "ssh node \"mongoexport -h mongo -d gpws -c $1 --jsonArray -o /data/backup/$backup_file\""
+  ssh node "mongoexport -h mongo -d gpws -c $1 --jsonArray -o /data/backup/$backup_file"
+
+  echo "scp node:/data/backup/$backup_file /data/backup/"
+  scp node:/data/backup/$backup_file /data/backup/
+
+  echo -e "mongoimport -d gpws -c $1 --jsonArray /data/backup/$backup_file"
+  mongoimport -d gpws -c $1 --jsonArray /data/backup/$backup_file
 }
