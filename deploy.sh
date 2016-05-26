342aa43c6dd7aeda421956068fb8e998198d16a3
lutaoact <i@lutao.me>
2016-05-26 16:57:17 +0800

    deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "[0;32m********************** DEV **********************[0m"
+DEV_CMD="cd ~/node-server && git pull && NODE_ENV=production pm2 restart app"
+echo "$DEV_CMD"
+ssh dev  "$DEV_CMD"
+
+PDT_CMD="cd ~/node-server9006 && git pull && pm2 restart app9006"
+echo "[0;32m********************** NODE1 **********************[0m"
+echo "$PDT_CMD"
+ssh node  "$PDT_CMD"
+
+echo "[0;32m********************** NODE2 **********************[0m"
+echo "$PDT_CMD"
+ssh node2 "$PDT_CMD"
--- read.sh
+++ read.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 read -p 'please input your first name: ' firstname
 read -p 'please input your last name: ' lastname
-echo -e "\nYour full name is: $firstname $lastname"
+echo "\nYour full name is: $firstname $lastname"
