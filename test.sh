adf318e08a2c68fcf3411123062951bbe1bdf06b
lutaoact <i@lutao.me>
2016-06-12 16:44:19 +0800

    test.sh while

--- test.sh
+++ test.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
 #echo "!hhh";
+#
+#if [ "$1" = 'all' ]; then
+#  echo 'all here'
+#else
+#  echo 'not all here'
+#fi
+#
 
-if [ "$1" = 'all' ]; then
-  echo 'all here'
-else
-  echo 'not all here'
-fi
+ts=$(date +'%s')
+
+while true
+do
+  echo $ts
+  dateStr=$(date -d @"$ts" +'%Y%m%d')
+  echo $dateStr
+
+  ts=$[ts - 86400]
+  if [ $dateStr -eq '20160321' ]; then
+    exit 0
+  fi
+done
