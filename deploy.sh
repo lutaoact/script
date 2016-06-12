0a8bc65d5d4e7958851591b29423290aa37b1706
lutaoact <i@lutao.me>
2016-06-12 16:46:16 +0800

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo "[0;32m********************** DEV **********************[0m"
-DEV_CMD="cd ~/node-server && git pull && NODE_ENV=production pm2 restart app"
+DEV_CMD="cd ~/node-server && git pull && pm2 restart app"
 echo "$DEV_CMD"
-ssh dev  "$DEV_CMD"
+ssh dev "$DEV_CMD"
 
 PDT_CMD="cd ~/node-server9006 && git pull && pm2 restart app9006"
 echo "[0;32m********************** NODE1 **********************[0m"
 echo "$PDT_CMD"
-ssh node  "$PDT_CMD"
+ssh node "$PDT_CMD"
 
 echo "[0;32m********************** NODE2 **********************[0m"
 echo "$PDT_CMD"
