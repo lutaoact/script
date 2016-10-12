7cb3f3d5a34d3c239c08fe0215e710d4899f51e4
lutaoact <i@lutao.me>
2016-10-12 18:07:36 +0800

    sync.sh: completed

--- sync.sh
+++ sync.sh
@@ -2,6 +2,7 @@
 
 ENV=$1
 
+## redis部分
 redis-cli -p 8888 shutdown
 
 if [ x"$1" == x"dev" ]; then
@@ -18,3 +19,8 @@ fi
 to_path=/data/redis/dump8888.rdb
 
 scp $host:"$from_path" "$to_path" && redis-server ~/guwan/conf/8888.conf
+
+## mongo部分
+# mongodump的-o选项指定的目录如果不存在会自动新建，导出数据时，会另外新建以database命名的目录来实际存储数据
+ssh $host "cd /tmp && mongodump -d guwan -o ./ && tar cvfz - guwan" \
+      | tar xvfz - -C /tmp && mongorestore -d guwan --drop --dir=/tmp/guwan
