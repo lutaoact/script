5fa26986c8be02b4de158a6ef627da927bd9be80
lutaoact <i@lutao.me>
2017-10-19 17:46:37 +0800

    watchAndRestart for make run_local_hub

--- findMakeThenKill.sh
+++ findMakeThenKill.sh
@@ -1,1 +1,3 @@
-ps axo 'user,pid,command' | grep 'make run_local_hms' | grep -v 'grep' | awk '{print $2}' | xargs kill
+#!/bin/bash
+# 兼容run_local_hms和run_local_hub
+ps axo 'user,pid,command' | grep 'make run_local_' | grep -v 'grep' | awk '{print $2}' | xargs kill
