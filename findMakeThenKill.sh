4e64344dd45388e9a934febcf58a36f1f67093d5
lutaoact <i@lutao.me>
2017-10-31 16:45:15 +0800

    watchAndRestart can work for hub and hms separately

--- findMakeThenKill.sh
+++ findMakeThenKill.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # 兼容run_local_hms和run_local_hub
-ps axo 'user,pid,command' | grep 'make run_local_' | grep -v 'grep' | awk '{print $2}' | xargs kill
+ps axo 'user,pid,command' | grep "make run_local_$1" | grep -v 'grep' | awk '{print $2}' | xargs kill
