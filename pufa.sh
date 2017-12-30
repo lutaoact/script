e3b497789f2a1c3a99916d6c5c0573bf1108275f
lutaoact <i@lutao.me>
2017-12-30 23:33:25 +0800

    fix pufa zhaohang

--- pufa.sh
+++ pufa.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-ack '0471.*支付宝 消费.*保险' /data/backup/pufa.txt | tee /data/backup/0471.txt | awk '{print substr($3, 4)}' | sed 's/,//g' | awk '{sum += $1 * 100}END{print sum, int(sum / 100)"."sum % 100}'
+ack '^0471.*支付宝' /data/backup/pufa.txt | tee /data/backup/0471.txt | awk '{print substr($3, 4)}' | sed 's/,//g' | awk '{sum += $1 * 100}END{printf "%d %d.%02d\n", sum, int(sum / 100), sum % 100}'
 
 # diff <(cat 0471.txt) <(grep 0471 tmp.txt)
 # diff <(cat 0471.txt) <(cat 0471.txt | ack '0471.*支付宝 消费.*保险')
 # 201710 55580.98
 # 201711 5967228 59672.28
+# 201712 3690282 36902.82
--- zhaohang.sh
+++ zhaohang.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-awk '{sum += $1 * 100}END{print sum, int(sum / 100)"."sum % 100}' /data/backup/zhaohang.txt
+awk '{sum += $1 * 100}END{printf "%d %d.%02d\n", sum, int(sum / 100), sum % 100}' /data/backup/zhaohang.txt
 
 # 201711 949082 9490.82
+# 201712 9491402 94914.02
