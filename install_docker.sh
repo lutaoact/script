0783b7ed1fe785e404a602cc0fed69ea8c33dbc1
lutaoact <i@lutao.me>
2018-02-18 16:42:05 +0800

    install_docker.sh

---
+++ install_docker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -xv
+
+set -n
+set -e
+
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+apt-key fingerprint 0EBFCD88
+
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+apt-get update && apt-get install docker-ce
