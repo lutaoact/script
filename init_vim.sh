be8bd12e0c8e753494f771c69dcb105d6d29c095
lutaoact <i@lutao.me>
2019-04-12 10:40:33 +0800

    init_vim.sh

--- init_vim.sh
+++ init_vim.sh
@@ -8,6 +8,8 @@ fi
 
 base_url="https://github.com"
 
-for i in (ctrlpvim/ctrlp.vim fatih/vim-go terryma/vim-multiple-cursors tpope/vim-surround wellle/targets.vim isRuslan/vim-es6); do
+for i in (ctrlpvim/ctrlp.vim fatih/vim-go terryma/vim-multiple-cursors tpope/vim-surround wellle/targets.vim isRuslan/vim-es6 SirVer/ultisnips); do
   install_vim_plugin "$base_url/$i"
 done
+
+cd ~/.vim && rm -rf ~/.vim/UltiSnips && git clone https://github.com/lutaoact/vim-snippets.git UltiSnips
